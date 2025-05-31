34ddd3194c36888ec5a20a015eb3441754dd0b16
Anis Rahman <anisr@Mac.local>
2025-05-31 23:53:12 +0300

    Minor - Fix DTW features with no values

--- src/distances.py
+++ src/distances.py
@@ -24,7 +24,7 @@ from multiprocessing import Pool, cpu_count
 
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 logger.setLevel(logging.DEBUG)
 
 
